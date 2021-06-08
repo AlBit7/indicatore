99f7b3c68181ed4873ce5cc84b582d0cedc34551
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-06-08 20:13:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-// const frecce = document.getElementsByTagName("svg");
+const frecce = document.getElementsByTagName("svg");
 
-// onmousemove = function (event) {
+var coordinateFrecce = new Array(frecce.length);
 
-//     let Xmouse = event.clientX;
-//     let Ymouse = event.clientY;
+for (let i = 0; i < frecce.length; ++i) {
 
-//     for (let i = 0; i < frecce.length; i++) {
+    let coordinate = frecce[i].getBoundingClientRect();
+    coordinateFrecce[i] = [(coordinate.left + coordinate.right) / 2, (coordinate.top + coordinate.bottom) / 2];
 
-//         let coordinate = frecce[i].getBoundingClientRect();
-//         let Xfreccia = (coordinate.left + coordinate.right) / 2;
-//         let Yfreccia = (coordinate.top + coordinate.bottom) / 2;
+}
 
-//         let angolo = Math.atan((Yfreccia - Ymouse) / (Xfreccia - Xmouse));
+onmousemove = (event) => {
 
-//         if (Xmouse <= Xfreccia)
-//             angolo = angolo - Math.PI;
+    for (let i = 0; i < frecce.length; ++i)
+        frecce[i].style = `transform: rotate(${(event.clientX <= coordinateFrecce[i][0]) ? Math.atan((coordinateFrecce[i][1] - event.clientY) / (coordinateFrecce[i][0] - event.clientX)) - Math.PI : Math.atan((coordinateFrecce[i][1] - event.clientY) / (coordinateFrecce[i][0] - event.clientX))}rad);`;
 
-//         frecce[i].style = 'transform: rotate(' + angolo + 'rad);';
+};
 
-//     }
+function coloreCasuale() {
 
-// };
+    // colori abbastanza accesi su sfondo giallo chiaro
+    let colori = ["green", "blue", "red", "black", "white", "teal", "slateblue", "royalblue", "orange", "violet", "prple", "peru", "tomato", "navy", "maroon", "lime", "lawngreen", "gold", "fuchsia", "firebrick", "crimson", "coral", "aqua"];
 
+    return colori[Math.floor(Math.random() * colori.length)];
 
+}
 
+function cambiaColore() {
 
+    for (let i = 0; i < frecce.length; ++i) {
+        colore = coloreCasuale();
+        for (let j = 0; j < 3; ++j)
+            frecce[i].getElementsByClassName("cls-1")[j].style.stroke = colore;
+    }
 
-
-
-
-
-
-
-
-
+}
 
 
 // const frecce = document.getElementsByTagName("svg");
@@ -68,75 +68,3 @@
 
 // };
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const frecce = document.getElementsByTagName("svg");
-
-// var coordinateFrecce = new Array(frecce.length);
-
-// for (let i = 0; i < frecce.length; ++i) {
-
-//     let coordinate = frecce[i].getBoundingClientRect();
-//     coordinateFrecce[i] = [(coordinate.left + coordinate.right) / 2, (coordinate.top + coordinate.bottom) / 2];
-
-// }
-
-// onmousemove = (event) => {
-
-//     let Xmouse = event.clientX;
-//     let Ymouse = event.clientY;
-
-//     for (let i = 0; i < frecce.length; ++i) {
-
-//         let Xfreccia = coordinateFrecce[i][0];
-//         let Yfreccia = coordinateFrecce[i][1];
-
-//         let angolo = (Xmouse <= Xfreccia) ? Math.atan((Yfreccia - Ymouse) / (Xfreccia - Xmouse)) - Math.PI : Math.atan((Yfreccia - Ymouse) / (Xfreccia - Xmouse));
-
-//         frecce[i].style = `transform: rotate(${angolo}rad);`;
-
-//     }
-
-// };
-
-
-
-
-
-
-
-
-
-
-const frecce = document.getElementsByTagName("svg");
-
-var coordinateFrecce = new Array(frecce.length);
-
-for (let i = 0; i < frecce.length; ++i) {
-
-    let coordinate = frecce[i].getBoundingClientRect();
-    coordinateFrecce[i] = [(coordinate.left + coordinate.right) / 2, (coordinate.top + coordinate.bottom) / 2];
-
-}
-
-onmousemove = (event) => {
-
-    for (let i = 0; i < frecce.length; ++i) {
-        
-        let angolo = (event.clientX <= coordinateFrecce[i][0]) ? Math.atan((coordinateFrecce[i][1] - event.clientY) / (coordinateFrecce[i][0] - event.clientX)) - Math.PI : Math.atan((coordinateFrecce[i][1] - event.clientY) / (coordinateFrecce[i][0] - event.clientX));
-
-        frecce[i].style = `transform: rotate(${angolo}rad);`;
-
-    }
-
-};
